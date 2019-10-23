1d28771fbaac6450794264926caff7cc61481122
Victor Wang <victor_wang@sottop.cn>
2019-10-23 16:45:25 +0800

    custom appId and appName

--- prompts.js
+++ prompts.js
@@ -1,5 +1,17 @@
 module.exports = [
     {
+        type: 'input',
+        name: 'appId',
+        message: 'input a appId',
+        default: 'com.example.app'
+    },
+    {
+        type: 'input',
+        name: 'appName',
+        message: 'input a appName',
+        default: 'ionic-vue-app'
+    },
+    {
         type: 'list',
         name: 'template',
         message: 'Please pick a preset',
